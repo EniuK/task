771e12eb80f68a2a393cc8b614d6c1bb320bf567
EniuK <eniuk113@gmail.com>
2023-12-18 18:17:44 +0100

    lets try

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,12 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
